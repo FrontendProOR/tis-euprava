8c233907f5ceeebaa42120f722f446adde1e4526
FrontendProOR <blackhatserbia@gmail.com>
2026-02-05 23:05:46 +0100

    frontend react project init file structure and docker and compose

---
+++ frontend/src/app/router.tsx
@@ -0,0 +1,10 @@
+import { createBrowserRouter } from 'react-router-dom'
+// import LoginPage from '../auth/pages/LoginPage'
+// import CitizenDashboard from '../citizen/pages/CitizenDashboard'
+// import AdminDashboard from '../admin/pages/AdminDashboard'
+
+export const router = createBrowserRouter([
+//   { path: '/', element: <LoginPage /> },
+//   { path: '/citizen', element: <CitizenDashboard /> },
+//   { path: '/admin', element: <AdminDashboard /> },
+])
